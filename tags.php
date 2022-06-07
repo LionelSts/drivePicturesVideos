15f17778301b2d35678496fea6bde779653381bf
augustin-m <60687546+augustin-m@users.noreply.github.com>
2022-06-07 10:00:25 +0200

    Composer + Tags

--- tags.php
+++ tags.php
@@ -6,8 +6,9 @@
 </head>
 
 <?php
+require 'vendor/autoload.php';
 session_start();
-$link = mysqli_connect("127.0.0.1", "root", "" , "drivelbr") ;
+$link = mysqli_connect("127.0.0.1", "root", "" , "drivelbr");
 $link->query('SET NAMES utf8');
 $requete = "SELECT `nom_categorie` FROM `categorie`";
 $result = mysqli_query($link, $requete);
@@ -28,33 +29,93 @@ while($row = mysqli_fetch_array($result)){
     ?>
     <div class="pageContent">
         <h1 class="bigTitle">Gestion des tags</h1>
-        <form class="profile" method="post" action="my_account-action.php">
+        <div class="profile">
             <label for="Categories" >Catégories : </label>
             <div class="tagsContainer">
 
                 <?php
                 foreach ($categorie as $value1) {
-                    echo('<div class="tagsLine">
+                    echo('<form method="post" action="tags-action.php"><div class="tagsLine">
                             <label for="nom" >'.$value1.'</label>
                             <input class="profile" type="text" id="nomCategorie" name="nomCategorie" value='.$value1.'>
-                            <input class="profile" type="submit" value="Modifier">
-                            <input class="profile" type="submit" value="Supprimer">
-                        </div>'
-                    );
+                            <input class="profile" type="submit" id='.$value1.' name="Modifier" value="Modifier">
+                            <input class="profile" type="submit" id='.$value1.' name="Supprimer" value="Supprimer">
+                        </div></form>');
                 }
                 ?>
             </div>
+            <form method="post" action="tags-action.php">
+                <div class="tagsLine">
+                    <label for='role'>Nouvelle catégorie :</label>
+                    <input class="profile" type="text" id="nomCategorie" name="nomCategorie1">
+                    <div class="lbrSelect">
+                        <select class="profile, role-select" onclick='formReload(mapAccounts)' id='account'>
+                            <?php
+                            $counter =0;
+                            $requete = "SELECT `nom_tag` FROM `tags` WHERE `nom_categorie` = 'Autre'";
+                            $result = mysqli_query($link, $requete);
+                            while($row = mysqli_fetch_array($result))
+                            {
+                                if($counter%2) echo '<option class="role-choices" value = `.$row["nom_tag"].`><input type="checkbox" id="mdp" name="mdp" value="mdp"><span class="redCheckbox"></span>'.$row["nom_tag"].'</option><br>';
+                                else echo '<option class="role-choices-1" value= `.$row["nom_tag"].`><input type="checkbox" id="mdp" name="mdp" value="mdp"><span class="redCheckbox"></span>'.$row["nom_tag"].'</option><br>';
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <input class="profile" type="submit" value="Créer">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<div class="pageContent">
+    <h1 class="bigTitle">Tags</h1>
+    <div class="profile">
+
+        <?php
+        $requete = "SELECT `nom_categorie`,`nom_tag` FROM `tags`";
+        $result = mysqli_query($link, $requete);
+        $data = mysqli_fetch_all($result);
+        $map = new \Ds\Map;
+        foreach ($data as $tag){
+            $tempArray = $map->get($tag[0], array());
+            $tempArray[] = $tag[1];
+            $map->put($tag[0],$tempArray);
+        }
+        foreach ($map as $key => $value){
+            echo '<label for="Categories" > '.$key.' </label><div class="tagsContainer">';
+            foreach ($value as $tag) {
+                echo '
+                              <form method="post" action="tags-action2.php">
+                                <div class="tagsLine">
+                                    <label for="nom" >'.$tag.'</label>
+                                    <input class="profile" type="text" id="nomCategorie" name="nomCategorie" value='.$tag.'>
+                                    <input class="profile" type="submit" id='.$tag.' name="Modifier" value="Modifier">
+                                    <input class="profile" type="submit" id='.$tag.' name="Supprimer" value="Supprimer">
+                                </div>
+                              </form>';
+            }
+            echo '</div>';
+        }
+        ?>
+        <form method="post" action="tags-action.php">
             <div class="tagsLine">
-                <label for='role'>Nouvelle catégorie :</label>
-                <input class="profile" type="text" id="nomCategorie" name="nomCategorie">
+                <label for='role'>Nouveau Tag :</label>
+                <input class="profile" type="text" id="nomCategorie" name="nomCategorie1">
                 <div class="lbrSelect">
-                    <select class="profile, role-select" name='role'
-                        <?php if($_SESSION['role'] != "admin") echo "disabled" ?>
-                    >
-                        <option class="role-choices" <?php if($_SESSION['role'] == "invite") echo "selected" ?> value='invite'>invité</option>
-                        <option class="role-choices-1" <?php if($_SESSION['role'] == "ecriture") echo "selected" ?> value='ecriture'>ecriture</option>
-                        <option class="role-choices" <?php if($_SESSION['role'] == "lecture") echo "selected" ?> value='lecture'>lecture</option>
-                        <option class="role-choices-1" <?php if($_SESSION['role'] == "admin") echo "selected" ?> value='admin'>admin</option>
+                    <select class="profile, role-select" onclick='formReload(mapAccounts)' id='account'>
+                        <?php
+                        $counter =0;
+                        $requete = "SELECT `nom_categorie` FROM `tags` GROUP By `nom_categorie`";
+                        $result = mysqli_query($link, $requete);
+                        while($row = mysqli_fetch_array($result))
+                        {
+                            if($counter%2) echo '<option class="role-choices" value = `.$row["nom_categorie"].`>'.$row["nom_categorie"].'</option><br>';
+                            else echo '<option class="role-choices-1" value= `.$row["nom_categorie"].`>'.$row["nom_categorie"].'</option><br>';
+                        }
+                        ?>
                     </select>
                 </div>
                 <input class="profile" type="submit" value="Créer">
@@ -62,41 +123,4 @@ while($row = mysqli_fetch_array($result)){
         </form>
     </div>
 </div>
-<div id="pageContent-bottom" class="pageContent">
-    <form class="profile" method="post" action="my_account-action.php">
-        <label for="Categories" >Tags : </label>
-        <div class="tagsContainer">
-
-            <?php
-            foreach ($categorie as $value1) {
-                echo('<div class="tagsLine">
-                            <label for="nom" >'.$value1.'</label>
-                            <input class="profile" type="text" id="nomCategorie" name="nomCategorie" value='.$value1.'>
-                            <input class="profile" type="submit" value="Modifier">
-                            <input class="profile" type="submit" value="Supprimer">
-                        </div>'
-                );
-            }
-            ?>
-        </div>
-        <div class="tagsLine">
-            <label for='role'>Nouvelle catégorie :</label>
-            <input class="profile" type="text" id="nomCategorie" name="nomCategorie">
-            <div class="lbrSelect">
-                <select class="profile, role-select" name='role'
-                    <?php if($_SESSION['role'] != "admin") echo "disabled" ?>
-                >
-                    <option class="role-choices" <?php if($_SESSION['role'] == "invite") echo "selected" ?> value='invite'>invité</option>
-                    <option class="role-choices-1" <?php if($_SESSION['role'] == "ecriture") echo "selected" ?> value='ecriture'>ecriture</option>
-                    <option class="role-choices" <?php if($_SESSION['role'] == "lecture") echo "selected" ?> value='lecture'>lecture</option>
-                    <option class="role-choices-1" <?php if($_SESSION['role'] == "admin") echo "selected" ?> value='admin'>admin</option>
-                </select>
-            </div>
-            <input class="profile" type="submit" value="Créer">
-        </div>
-    </form>
-</div>
-</div>
 </body>
-
-
