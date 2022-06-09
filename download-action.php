38945f3111618b67acf8793bde53e4009eee469d
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2022-06-09 16:18:37 +0200

    Fix zip subfolder

--- download-action.php
+++ download-action.php
@@ -12,8 +12,8 @@ $path = '/temporary/'.$fileName;
 if ($zip->open($_SERVER['DOCUMENT_ROOT']."/driveBriquesRouges/temporary/".$fileName, ZipArchive::CREATE) === TRUE)
 {
     foreach ($files as $file){
-        $file = "./fichiers/".$file;
-        $zip->addFile($file);
+        $filePath = "./fichiers/".$file;
+        $zip->addFile($filePath, $file);
     }
     // All files are added, so close the zip file.
     $zip->close();
@@ -37,7 +37,7 @@ if (headers_sent()) {
         header('Content-Type: application/zip');
         header("Content-Transfer-Encoding: Binary");
         header('Content-Length: ' . filesize($fileToSend));
-        header("Content-Disposition: attachment; filename=\"" . basename($fileToSend) . "\"");
+        header("Content-Disposition: attachment; filename=\"" . basename($fileName) . "\"");
         while (ob_get_level()) {
             ob_end_clean();
         }
